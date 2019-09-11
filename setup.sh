23136975564f74e565e963e41c48fd60a55b1058
Tim Johns <12250654+tmjohns@users.noreply.github.com>
2019-09-11 19:24:42 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ sudo yum install npm -y
 npm install -g c9
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py --user
-sudo pip install -r requirements.txt
+pip3 install -r requirements.txt --user
 
 echo "==========> Dependencies installed successfully."
